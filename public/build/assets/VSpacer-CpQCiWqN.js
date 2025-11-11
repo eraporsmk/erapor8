8b140025df3673f975688b143bd97465a87db837
masadi <masadi.com@gmail.com>
2025-11-11 13:42:17 +0700

    Ganti nama aplikasi

--- public/build/assets/VSpacer-CpQCiWqN.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{c as r}from"./VAvatar-DnYuN54U.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ public/build/assets/useAbility-LZkiB6CV.js
@@ -0,0 +1,1 @@
+import{ar as i}from"./main-CcRoSAQ_.js";const t=()=>i();export{t as u};
